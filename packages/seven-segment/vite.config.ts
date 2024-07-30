34534067389d00deaca7745bc6bf5450aedd7eb3
김민우 <0chickim@gmail.com>
2024-07-30 20:46:04 +0900

    feat(seven-segment): seven-segment 프로젝트 생성

---
+++ packages/seven-segment/vite.config.ts
@@ -0,0 +1,32 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ["lib/**/!(*.spec|*.test).{ts,tsx}"],
+      rollupTypes: true,
+      tsconfigPath: resolve(__dirname, "tsconfig.build.json"),
+    }),
+  ],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
+    },
+  },
+  test: {
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
+  },
+});
