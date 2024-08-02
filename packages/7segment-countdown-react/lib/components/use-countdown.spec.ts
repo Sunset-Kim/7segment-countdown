c701a02b8514856b6cb0c477f831659251326fe3
김민우 <0chickim@gmail.com>
2024-08-03 00:05:04 +0900

    fix(7segment-countdown-react): 더이상 쓰지 않는 훅테스트렌더를 제거

--- packages/7segment-countdown-react/lib/components/use-countdown.spec.ts
+++ packages/7segment-countdown-react/lib/components/use-countdown.spec.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
-import { renderHook, act } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react";
 import { useCountdown } from "./use-countdown";
 
 describe("useCountdown", () => {
