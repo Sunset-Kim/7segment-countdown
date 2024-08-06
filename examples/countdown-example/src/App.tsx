8c738a5ae3dc1c8845bde7e042c5c4222b2d08e6
김민우 <0chickim@gmail.com>
2024-08-06 20:39:40 +0900

    feat(example): 예제코드 추가

--- examples/countdown-example/src/App.tsx
+++ examples/countdown-example/src/App.tsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import "./App.css";
-import { Countdown, SevenSegment } from "@7segment/countdown-react";
+import {
+  Colon,
+  Countdown,
+  SegmentDisplay,
+  SevenSegment,
+  useControlledCountdown,
+} from "@7segment/countdown-react";
 
 function App() {
   const [count, setCount] = useState(0);
   const [targetDate, setTargetDate] = useState(
     new Date("2024-08-09T00:00:00+09:00")
   );
+  const { days, hours, minutes, seconds, start, stop, reset, isRunning } =
+    useControlledCountdown(60 * 60 * 24 * 1);
 
   return (
     <>
       <div>
-        <h1>7 Segment</h1>
+        <h1>Basic 7 Segment</h1>
       </div>
 
       <div className="card">
@@ -26,7 +34,7 @@ function App() {
           <button onClick={() => setCount((count) => count + 1)}>
             Increase
           </button>
-          {count}
+          <h2>{count}</h2>
           <SevenSegment digit={count} />
           <button onClick={() => setCount((count) => count - 1)}>
             Decrease
@@ -37,14 +45,40 @@ function App() {
       <div>
         <h1>7 Segment Countdown</h1>
       </div>
-      <div style={{ marginBottom: 20 }}>
-        <input
-          type="datetime-local"
-          value={targetDate.toISOString().slice(0, 16)}
-          onChange={(e) => setTargetDate(new Date(e.target.value))}
-        />
+      <div>
+        <div style={{ marginBottom: 20 }}>
+          <input
+            type="datetime-local"
+            value={targetDate.toISOString().slice(0, 16)}
+            onChange={(e) => setTargetDate(new Date(e.target.value))}
+          />
+        </div>
+        <Countdown targetDate={targetDate} size={20} />
       </div>
-      <Countdown targetDate={targetDate} size={20} />
+
+      <div>
+        <h1>7 Segment Countdown Timer</h1>
+      </div>
+      <div>
+        <div>
+          <SegmentDisplay value={days} />
+          <Colon />
+          <SegmentDisplay value={hours} />
+          <Colon />
+          <SegmentDisplay value={minutes} />
+          <Colon />
+          <SegmentDisplay value={seconds} />
+        </div>
+        <div>
+          <button onClick={start} disabled={isRunning}>
+            Start
+          </button>
+          <button onClick={stop} disabled={!isRunning}>
+            Stop
+          </button>
+          <button onClick={reset}>Reset</button>
+        </div>
+      </div>
     </>
   );
 }
