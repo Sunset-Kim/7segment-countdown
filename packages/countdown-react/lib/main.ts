6640c1b3e85902cacbe76fd84408a46ac71026bc
김민우 <0chickim@gmail.com>
2024-08-06 20:39:18 +0900

    feat: segment를 더 편하게 사용하는 컴포넌트 생성

--- packages/countdown-react/lib/main.ts
+++ packages/countdown-react/lib/main.ts
@@ -2,6 +2,7 @@ export { Colon } from "./components/colon";
 export { Dot } from "./components/dot";
 export { Countdown } from "./components/countdown";
 export { SevenSegment } from "./components/seven-segment";
+export { SegmentDisplay } from "./components/segment-display";
 
 export { useCountdown } from "./hooks/use-countdown";
 export { useControlledCountdown } from "./hooks/use-controlled-countdown";
