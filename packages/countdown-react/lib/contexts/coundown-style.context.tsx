125658c2a540db8410fe65b669168961a555d6ce
김민우 <0chickim@gmail.com>
2024-08-06 20:47:44 +0900

    refactor(react): 필요없는 컨텍스트 코드를 삭제

--- packages/countdown-react/lib/contexts/coundown-style.context.tsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-import { SevenSegmentProps } from "../components/seven-segment";
-import { ColonProps } from "../components/colon";
-
-export const CountdownStyleContext = createContext<
-  Omit<SevenSegmentProps, "digit"> & ColonProps
->({});
-
-export const CountdownStyleProvider = CountdownStyleContext.Provider;
